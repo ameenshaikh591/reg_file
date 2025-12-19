ffa488e972258e0ca8490f0605f54a09cf944dbc
Shaikh <ashaikh81@ece-linlabsrv01.ece.gatech.edu>
2025-12-18 20:21:41 -0500

    Rename ports for better clarity

--- reg_file.sv
+++ reg_file.sv
@@ -1,19 +1,22 @@
 module reg_file (
-    parameter REG_WIDTH = 32,
-    parameter REG_ADDR_LENGTH = 8
+    parameter int REG_WIDTH = 32,
+    parameter int ADDR_WIDTH = 8
 ) (
     input logic i_clk,
 
-    input logic [REG_ADDR_LENGTH - 1 :0] i_reg_a_addr_r,
-    input logic [REG_ADDR_LENGTH - 1 : 0] i_reg_b_addr_r,
+    input logic [ADDR_WIDTH - 1 :0] i_reg_a_addr_r,
+    input logic [ADDR_WIDTH - 1 : 0] i_reg_b_addr_r,
 
-    input logic [REG_ADDR_LENGTH - 1 : 0] i_reg_addr_w,
+    input logic [ADDR_WIDTH - 1 : 0] i_reg_addr_w,
     input logic [REG_WIDTH - 1 : 0] i_reg_val_w,
     input logic i_write_en,
 
-    output logic [REG_WIDTH - 1 : 0] o_reg_a_r,
-    output logic [REG_WIDTH - 1 : 0] o_reg_b_r
+    output logic [REG_WIDTH - 1 : 0] o_reg_a_val_r,
+    output logic [REG_WIDTH - 1 : 0] o_reg_b_val_r
 );
+
+
+    logic [REG_WIDTH - 1 : 0] registers []
 
 
 endmodule
