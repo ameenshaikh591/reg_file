4f2b6c18a5e127998ce8c0270ce64b733561732a
ameenshaikh591 <ameenshaikh591@gmail.com>
2025-12-18 20:30:03 -0500

    Module logic (ambiguous read/write first)

--- reg_file.sv
+++ reg_file.sv
@@ -1,10 +1,10 @@
 module reg_file (
     parameter int REG_WIDTH = 32,
-    parameter int ADDR_WIDTH = 8
+    parameter int ADDR_WIDTH = 4
 ) (
     input logic i_clk,
 
-    input logic [ADDR_WIDTH - 1 :0] i_reg_a_addr_r,
+    input logic [ADDR_WIDTH - 1 : 0] i_reg_a_addr_r,
     input logic [ADDR_WIDTH - 1 : 0] i_reg_b_addr_r,
 
     input logic [ADDR_WIDTH - 1 : 0] i_reg_addr_w,
@@ -14,9 +14,19 @@ module reg_file (
     output logic [REG_WIDTH - 1 : 0] o_reg_a_val_r,
     output logic [REG_WIDTH - 1 : 0] o_reg_b_val_r
 );
+    localparam int NUM_REGS = 1 << ADDR_WIDTH;
 
+    logic [REG_WIDTH - 1 : 0] registers [NUM_REGS - 1 : 0];
 
-    logic [REG_WIDTH - 1 : 0] registers []
+    always_comb begin
+        o_reg_a_val_r = registers[i_reg_a_addr_r];
+        o_reg_b_val_r = registers[i_reg_b_addr_r];
+    end
 
+    always_ff @(posedge i_clk) begin
+        if (i_write_en) begin
+            registers[i_reg_addr_w] <= i_reg_val_w;
+        end
+    end
 
 endmodule
