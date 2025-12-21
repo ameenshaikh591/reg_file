d5cb4446d1cd62f5e441c656c0f658884ef7a40f
ameenshaikh591 <ameenshaikh591@gmail.com>
2025-12-21 17:54:18 -0500

    Remove xcelium and simvision directories (test)

--- xcelium.d/run.lnx8664.25.03.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1766357072 /nethome/ashaikh81/Personal/reg_file/reg_file_tb.sv
-1766203183 /nethome/ashaikh81/Personal/reg_file/reg_file.sv
--- xcelium.d/run.lnx8664.25.03.d/files.ts
+++
@@ -1,3 +0,0 @@
-1753467282 /tools/software/cadence/ic/23.10.150/share/cdssetup/cds.lib
-1766357072 /nethome/ashaikh81/Personal/reg_file/reg_file_tb.sv
-1766203183 /nethome/ashaikh81/Personal/reg_file/reg_file.sv
